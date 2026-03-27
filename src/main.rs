7dac7cd7623696debf03ca088ce9693fa798e0a3
Nicola Orlando <niki.or97@yahoo.it>
2026-03-27 12:23:22 +0100

    Remove socket if it exists

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,53 @@
 use std::error::Error;
-use std::os::unix::net::UnixListener;
+use std::os::unix::net::{UnixListener, UnixStream};
 use std::thread;
 
 mod ftp;
 
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
-    // TODO: remove socket if it exists
     let dir = std::env::var("WP360_CODESYS_SOCK_DIR")
         .unwrap_or("/var/opt/codesyscontrolapi/extfuncs/".to_string());
 
-    let sock_path = std::path::Path::new(&dir);
-    if !sock_path.is_dir() {
+    let dir_path = std::path::Path::new(&dir);
+    if !dir_path.is_dir() {
         return Err(format!(
             "Invalid path - \"{}\" does not exist or is not a directory",
             dir
         )
         .into());
     }
+    let sock_path = dir_path.join("wp360-ftp.sock");
+    match sock_path.try_exists() {
+        Ok(true) => {
+            match UnixStream::connect(&sock_path) {
+                Ok(stream) => {
+                    // Socket is in use
+                    stream.shutdown(std::net::Shutdown::Both)?;
+                    // Gonna let the ::bind call throw an error instead
+                }
+                Err(_e) => {
+                    // Socket is not in use or at the very least not connectable, safe to remove
+                    std::fs::remove_file(&sock_path)?;
+                }
+            }
+        }
+        Ok(false) => {}
+        Err(e) => return Err(Box::new(e)),
+    }
 
-    let listener = UnixListener::bind(sock_path.join("wp360-ftp.sock"))?; // TODO: handle error, and fix the path
+    let listener = UnixListener::bind(&sock_path)?;
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 thread::spawn(|| ftp::client(stream));
             }
-            Err(_err) => {
-                break; // TODO: we probably don't _want_ to break? then again, depending on the error, we might need to
+            Err(err) => {
+                std::fs::remove_file(&sock_path)?;
+                return Err(Box::new(err));
             }
         }
     }
-    // TODO: remove socket
     Ok(())
 }
