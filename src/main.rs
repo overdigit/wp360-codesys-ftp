a42d88677769e785491bc5882dbe923fb68726cd
Nicola Orlando <niki.or97@yahoo.it>
2026-04-03 12:23:31 +0200

    Wait for the default folder to be created if it doesn't exist. This works around the fact codesyscontrol creates a permanent folder at runtime at first boot instead of including it in the .deb file.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
+use inotify::{Inotify, WatchMask, EventMask};
 use std::error::Error;
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::thread;
 
 mod ftp;
 
+// Credit: https://github.com/nroi/waitforfile/blob/master/src/main.rs
+fn wait_for(dirname: &std::path::Path) -> Result<bool, Box<dyn Error>> {
+    let mut ino = Inotify::init()?;
+    let parent = dirname.parent().expect("This folder has no parent. How? It's the default folder, I wrote its path. What did you do?");
+    ino.watches().add(parent, WatchMask::DELETE_SELF | WatchMask::CREATE).unwrap();
+    if !dirname.exists() {
+        loop {
+            let mut buffer = [0; 1024];
+            let events = ino.read_events_blocking(&mut buffer)
+                .expect("Error while reading events");
+            for event in events {
+                match event.name {
+                    Some(name) => {
+                        if parent.join(name) == dirname {
+                            return Ok(true);
+                        }
+                    },
+                    None => {
+                        if event.mask == EventMask::DELETE_SELF {
+                            return Err("The watched directory has been deleted.".into());
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // file already exists prior to running this program.
+    Ok(false)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
-    let dir = std::env::var("WP360_CODESYS_SOCK_DIR")
-        .unwrap_or("/var/opt/codesyscontrolapi/extfuncs/".to_string());
+    let dir_env = std::env::var("WP360_CODESYS_SOCK_DIR");
+    let env_set = dir_env.is_ok();
+    let dir = dir_env.unwrap_or("/var/opt/codesyscontrolapi/extfuncs/".to_string());
 
     let dir_path = std::path::Path::new(&dir);
     if !dir_path.is_dir() {
-        return Err(format!(
-            "Invalid path - \"{}\" does not exist or is not a directory",
-            dir
-        )
-        .into());
+        if env_set {
+            return Err(format!(
+                "Invalid path - \"{}\" does not exist or is not a directory",
+                dir
+            ).into());
+        } else {
+            let path = std::path::Path::new(&dir);
+            wait_for(path)?;
+        }
     }
     let sock_path = dir_path.join("wp360-ftp.sock");
     match sock_path.try_exists() {
